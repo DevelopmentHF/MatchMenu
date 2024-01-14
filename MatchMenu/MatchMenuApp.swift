7e872af5921c0be4e3135839af5c14843abcd276
developmentHF <development.henry@gmail.com>
2024-01-14 14:06:00 +1100

    Initial Commit

---
+++ MatchMenu/MatchMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMenuApp.swift
+//  MatchMenu
+//
+//  Created by Henry Fielding on 14/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
