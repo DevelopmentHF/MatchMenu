b5c66e911bdbd2699b2afd268d77fd764b628232
developmentHF <development.henry@gmail.com>
2024-01-14 18:16:03 +1100

    reading json file

--- MatchMenu/ContentView.swift
+++ MatchMenu/ContentView.swift
@@ -11,13 +11,42 @@ struct ContentView: View {
     
     @State var selectedTeam: Team = .ManchesterUnited
     
+    @State private var apiData: [String: Any]?
+    
     var body: some View {
         VStack {
             PickerView(selectedTeam: $selectedTeam)
             Divider()
             NextFixtureView(selectedTeam: $selectedTeam)
+            Divider()
+            Button("read json") {
+                readJSONFromFile()
+            }
         }
         .padding()
+    }
+    
+    func readJSONFromFile() {
+        // Get the file URL for the JSON file in your app bundle
+        if let fileURL = Bundle.main.url(forResource: "PL_2023-24_Fixture", withExtension: "json") {
+            do {
+                // Read the JSON data from the file
+                let data = try Data(contentsOf: fileURL)
+                
+                // Parse the JSON data
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                    // Now 'json' is a dictionary containing your JSON data
+                    print(json)
+
+                } else {
+                    print("Failed to parse JSON")
+                }
+            } catch {
+                print("Error reading JSON file: \(error.localizedDescription)")
+            }
+        } else {
+            print("JSON file not found in the app bundle")
+        }
     }
 }
 
